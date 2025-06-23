cda84dbe1622d1f6cd1a54481e5a1d3aa2d4c98a
saeed_811 <yzerwata@gmail.com>
2025-06-23 18:45:41 +0100

    Add files via upload

---
+++ said.js
@@ -0,0 +1,37 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlides() {
+  slides.forEach(slide => slide.style.display = "none");
+  slideIndex++;
+  if (slideIndex > slides.length) slideIndex = 1;
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000); // كل 3 ثواني
+}
+
+showSlides();
+
+window.addEventListener("scroll", () => {
+  const teamBoxes = document.querySelectorAll(".team-box");
+  teamBoxes.forEach((el, i) => {
+    const rect = el.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      setTimeout(() => {
+        el.classList.add("show");
+      }, i * 200);
+    }
+  });
+});
+
+// Animation services on scroll
+window.addEventListener("scroll", () => {
+  const serviceCards = document.querySelectorAll(".service-card");
+  serviceCards.forEach((card, i) => {
+    const rect = card.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      setTimeout(() => {
+        card.classList.add("show");
+      }, i * 200);
+    }
+  });
+});
